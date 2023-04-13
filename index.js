351584e56fd45864ae904cb042ae46932e2b690f
Ikechukwu Festus-Ihedioha <iihediohaa@gmail.com>
2023-04-13 03:11:18 +0000

    Updated Movies Endpoints

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import moviesRouter from './src/movies';
 import genresRouter from './src/genres';
 import createAccountsRouter from './src/accounts/routes';
 import buildDependencies from "./src/config/dependencies";
+import createMoviesRouter from './src/movies/routes';
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', createMoviesRouter(dependencies));
 
 app.use('/api/genres', genresRouter);
 
