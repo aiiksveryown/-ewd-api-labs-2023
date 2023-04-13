0a9ee8c576de0a7d5472c5b2075e8e2deb620d8d
Ikechukwu Festus-Ihedioha <iihediohaa@gmail.com>
2023-04-13 03:04:00 +0000

    Added Accounts Endpoints

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './src/movies';
 import genresRouter from './src/genres';
+import createAccountsRouter from './src/accounts/routes';
+import buildDependencies from "./src/config/dependencies";
 
 dotenv.config();
 
 const app = express();
+
+const dependencies = buildDependencies();
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT;
@@ -15,6 +19,8 @@ app.use(express.json());
 app.use('/api/movies', moviesRouter);
 
 app.use('/api/genres', genresRouter);
+
+app.use('/api/accounts', createAccountsRouter(dependencies));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
