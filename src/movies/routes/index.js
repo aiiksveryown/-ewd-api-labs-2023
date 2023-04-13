b9fd7955ac9a1009cdf54e2efaac5399ac0b609d
Ikechukwu Festus-Ihedioha <iihediohaa@gmail.com>
2023-04-13 17:48:35 +0000

    Finishes Exercises for Node Lab2

--- src/movies/routes/index.js
+++ src/movies/routes/index.js
@@ -2,16 +2,19 @@ import express from 'express';
 import MoviesController from '../controllers';
 
 const createMoviesRouter = (dependencies) => {
-    const router = express.Router();
-    // load controllers with dependencies
-    const moviesController = MoviesController(dependencies);
-
-    router.route('/:id')
-        .get(moviesController.getMovie);
-
-    router.route('/')
-        .get(moviesController.find);
-
-    return router;
+  const router = express.Router();
+  // load controllers with dependencies
+  const moviesController = MoviesController(dependencies);
+  
+  router.route('/:id')
+    .get(moviesController.getMovie);
+  
+  router.route('/')
+    .get(moviesController.find);
+  
+  router.route('/upcoming')
+    .get(moviesController.getUpcomingMovies);
+  
+  return router;
 };
 export default createMoviesRouter;
